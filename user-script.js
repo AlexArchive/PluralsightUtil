8baae314974ad2bc2ea25ac301464f1559aa51d9
szdc <jack.willis-craig@hotmail.com>
2014-12-05 21:57:48 +1100

    Wrapped the script in a function to enable strict mode throughout.

--- user-script.js
+++ user-script.js
@@ -9,35 +9,39 @@
 // @include      *pluralsight.com/profile*
 // ==/UserScript==
 
-function tallyHoursWatched(callback) {
-    var durationWatched = moment.duration(0);
-    $.get("http://www.pluralsight.com/data" + window.location.pathname, function(profile) {
-        var courses = profile.usageInfo.completedCourses;
-        $.each(courses, function(index, course) {
-            $.get("http://www.pluralsight.com/data/course/" + course.courseName, function(course) {
-                durationWatched.add(course.duration);
-                if (index === courses.length - 1) {
-                    callback(durationWatched.asHours());
-                }
-            });
-        });
-    });
-}
+(function () {
+  'use strict';
+  
+  function tallyHoursWatched(callback) {
+      var durationWatched = moment.duration(0);
+      $.get("http://www.pluralsight.com/data" + window.location.pathname, function(profile) {
+          var courses = profile.usageInfo.completedCourses;
+          $.each(courses, function(index, course) {
+              $.get("http://www.pluralsight.com/data/course/" + course.courseName, function(course) {
+                  durationWatched.add(course.duration);
+                  if (index === courses.length - 1) {
+                      callback(durationWatched.asHours());
+                  }
+              });
+          });
+      });
+  }
 
-function onCalculateLinkClicked() {
-    $(".calculate-link").text("Calculating..");
-    tallyHoursWatched(function(hoursWatched) {
-        alert("You have watched " + hoursWatched + " hours of Pluralsight videos");
-        $(".calculate-link").text("Calculate hours-watched");
-    });
-}
+  function onCalculateLinkClicked() {
+      $(".calculate-link").text("Calculating..");
+      tallyHoursWatched(function(hoursWatched) {
+          alert("You have watched " + hoursWatched + " hours of Pluralsight videos");
+          $(".calculate-link").text("Calculate hours-watched");
+      });
+  }
 
-function onHeaderRendered(element) {
-    element
-        .next("ul")
-        .append("<li><a class=\"calculate-link\">Calculate hours-watched</a></li>");
+  function onHeaderRendered(element) {
+      element
+          .next("ul")
+          .append("<li><a class=\"calculate-link\">Calculate hours-watched</a></li>");
 
-    $(".calculate-link").click(onCalculateLinkClicked);
-}
+      $(".calculate-link").click(onCalculateLinkClicked);
+  }
 
-waitForKeyElements("h6:contains(\"More info\")", onHeaderRendered);
+  waitForKeyElements("h6:contains(\"More info\")", onHeaderRendered);
+})();
